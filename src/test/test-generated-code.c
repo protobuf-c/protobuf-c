a9c16cc8394117bcff9852f551fe5e555b2bef39
lahiker42 <lahiker42@00440858-1255-0410-a3e6-75ea37f81c3a>
2008-08-22 22:15:42 +0000

    git-svn-id: https://protobuf-c.googlecode.com/svn/trunk@21 00440858-1255-0410-a3e6-75ea37f81c3a

---
+++ src/test/test-generated-code.c
@@ -0,0 +1,39 @@
+#include "generated-code/test.pb-c.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  Foo__Person person;
+  Foo__Person *person2;
+  unsigned char simple_pad[8];
+  size_t size, size2;
+  unsigned char *packed;
+  ProtobufCBufferSimple bs = PROTOBUF_C_BUFFER_SIMPLE_INIT (simple_pad);
+
+  memset (&person, 0, sizeof (person));
+  person.descriptor = &foo__person__descriptor;
+  person.name = "dave b";
+  person.id = 42;
+  size = foo__person__get_packed_size (&person);
+  packed = malloc (size);
+  assert (packed);
+  size2 = foo__person__pack (&person, packed);
+  assert (size == size2);
+  foo__person__pack_to_buffer (&person, &bs.base);
+  assert (bs.len == size);
+  assert (memcmp (bs.data, packed, size) == 0);
+  PROTOBUF_C_BUFFER_SIMPLE_CLEAR (&bs);
+  person2 = foo__person__unpack (NULL, size, packed);
+  assert (person2 != NULL);
+  assert (person2->id == 42);
+  assert (strcmp (person2->name, "dave b") == 0);
+
+  foo__person__free_unpacked (person2, NULL);
+  free (packed);
+
+  printf ("test succeeded.\n");
+
+  return 0;
+}
