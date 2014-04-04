c11a598e3cccd34465b8b602b9b7be51df7f1c6a
Robert Edmonds <edmonds@fsi.io>
2014-04-04 19:23:42 -0400

    protobuf-c: bump PROTOBUF_C_VERSION to 1.0.0-rc1

--- protobuf-c/protobuf-c.h
+++ protobuf-c/protobuf-c.h
@@ -95,7 +95,7 @@ protobuf_c_version_number(void);
  * The version of the protobuf-c headers, represented as a string using the same
  * format as protobuf_c_version().
  */
-#define PROTOBUF_C_VERSION		"1.0.0-pre"
+#define PROTOBUF_C_VERSION		"1.0.0-rc1"
 
 /**
  * The version of the protobuf-c headers, represented as an integer using the
