ef89c7480ff0a2f3fcc16c98a2b7d237947bb33b
Kevin Lyda <kevin@ie.suberic.net>
2014-04-06 00:18:02 +0100

    Further public API docs.

--- protobuf-c/protobuf-c.h
+++ protobuf-c/protobuf-c.h
@@ -37,9 +37,8 @@
  *
  * \authors Dave Benson and the protobuf-c authors
  * \date 2008-2014
- */
-
-/** \mainpage
+ *
+ * \mainpage
  *
  * \section description Description
  *
@@ -60,11 +59,6 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <limits.h>
-
-/** \defgroup api Public API for libprotobuf-c
- *
- * The public API for C protobufs.
- * @{ */
 
 #ifdef __cplusplus
 # define PROTOBUF_C_BEGIN_DECLS	extern "C" {
@@ -98,10 +92,26 @@
 #endif
 
 PROTOBUF_C_BEGIN_DECLS
+/** \defgroup api Public API for libprotobuf-c
+ *
+ * The public API for C protobufs.  These interfaces are stable.
+ *
+ * <b>Packed Messages</b>
+ *
+ * To pack a message, you have two options:
+ *
+ *   -# you can compute the size of the message using
+ *      protobuf_c_message_get_packed_size() then pass
+ *      protobuf_c_message_pack() a buffer of that length.
+ *   -# Provide a virtual buffer (a ProtobufCBuffer) to
+ *      accept data as we scan through it.
+ * @{ */
 
-/**
- * Get the version of the protobuf-c library. Note that this is the version of
- * the library linked against, not the version of the headers compiled against.
+/** Get the version of the library.
+ *
+ * Get the version of the protobuf-c library. Note that this is the
+ * version of the library linked against, not the version of the
+ * headers compiled against.
  *
  * \return A string containing the version number of protobuf-c.
  */
@@ -109,37 +119,63 @@ PROTOBUF_C_API
 const char *
 protobuf_c_version(void);
 
-/**
- * Get the version of the protobuf-c library. Note that this is the version of
- * the library linked against, not the version of the headers compiled against.
+/** Get numeric version of the library.
+ *
+ * Get the version of the protobuf-c library. Note that this is the
+ * version of the library linked against, not the version of the
+ * headers compiled against.
  *
  * \return A 32 bit unsigned integer containing the version number of
- *	protobuf-c, represented in base-10 as (MAJOR*1E6) + (MINOR*1E3) + PATCH.
+ *	   protobuf-c, represented in base-10 as
+ *	   (MAJOR*1E6) + (MINOR*1E3) + PATCH.
  */
 PROTOBUF_C_API
 uint32_t
 protobuf_c_version_number(void);
 
-/**
- * The version of the protobuf-c headers, represented as a string using the same
- * format as protobuf_c_version().
+/** The version of the library as a #define.
+ *
+ * The version of the protobuf-c headers, represented as a string using
+ * the same format as protobuf_c_version().
  */
 #define PROTOBUF_C_VERSION		"1.0.0-pre"
 
-/**
- * The version of the protobuf-c headers, represented as an integer using the
- * same format as protobuf_c_version_number().
+/** The numeric version of the library as a #define.
+ *
+ * The version of the protobuf-c headers, represented as an integer
+ * using the same format as protobuf_c_version_number().
  */
 #define PROTOBUF_C_VERSION_NUMBER	1000000
 
+/** Boolean type used in protobuf-c. */
 typedef int protobuf_c_boolean;
 
+/** Enumeration of the rules for this field.
+ *
+ * A field will be required, optional or repeated.  These enumerations
+ * cover those options.
+ *
+ * \sa
+ *   - Google protobuf docs on how to define a field:
+ *     https://developers.google.com/protocol-buffers/docs/proto#simple
+ *   - How to add new fields to a message:
+ *     https://developers.google.com/protocol-buffers/docs/proto#updating
+ */
 typedef enum {
 	PROTOBUF_C_LABEL_REQUIRED,
 	PROTOBUF_C_LABEL_OPTIONAL,
 	PROTOBUF_C_LABEL_REPEATED
 } ProtobufCLabel;
 
+/** Enumeration of the field types.
+ *
+ * This covers the list of all message types.
+ *
+ * \sa
+ *   - Scalar types.  Further on enumeration and nested message types
+ *     are covered.
+ *     https://developers.google.com/protocol-buffers/docs/proto#scalar
+ */
 typedef enum {
 	PROTOBUF_C_TYPE_INT32,
 	PROTOBUF_C_TYPE_SINT32,
@@ -161,6 +197,11 @@ typedef enum {
 	PROTOBUF_C_TYPE_MESSAGE,
 } ProtobufCType;
 
+/** Data structure for \c bytes type.
+ *
+ * Byte fields might not be ASCII \c NUL terminated strings.  They might
+ * have embedded \c NULs or they might not have a \c NUL at all.
+ */
 typedef struct _ProtobufCBinaryData ProtobufCBinaryData;
 struct _ProtobufCBinaryData {
 	size_t	len;
@@ -170,16 +211,24 @@ struct _ProtobufCBinaryData {
 typedef struct _ProtobufCIntRange ProtobufCIntRange; /* private */
 
 /* --- memory management --- */
+/** Functions for managing memory.
+ *
+ * Used by the protocol_c family of functions to manage memory.
+ */
 typedef struct _ProtobufCAllocator ProtobufCAllocator;
 struct _ProtobufCAllocator
 {
 	void		*(*alloc)(void *allocator_data, size_t size);
+	/**< Function to allocate memory. */
 	void		(*free)(void *allocator_data, void *pointer);
+	/**< Function to free memory. */
 	void		*allocator_data;
+	/**< Opaque buffer that the alloc and free functions
+         * might use to save state. */
 };
 
-/*
- * This is a configurable allocator.
+/** This is a configurable default allocator.
+ *
  * By default, it uses the system allocator (meaning malloc() and free()).
  * This is typically changed to adapt to frameworks that provide
  * some nonstandard allocation functions.
@@ -189,6 +238,11 @@ struct _ProtobufCAllocator
 extern PROTOBUF_C_API ProtobufCAllocator protobuf_c_default_allocator; /* settable */
 
 /* --- append-only data buffer --- */
+/** Append-only data buffer.
+ *
+ * Used by the protobuf_c_message_pack_to_buffer() function to store
+ * the packed message.
+ */
 typedef struct _ProtobufCBuffer ProtobufCBuffer;
 struct _ProtobufCBuffer {
 	void		(*append)(ProtobufCBuffer *buffer,
@@ -202,29 +256,19 @@ typedef struct _ProtobufCEnumValue ProtobufCEnumValue;
 typedef struct _ProtobufCEnumValueIndex ProtobufCEnumValueIndex;
 typedef struct _ProtobufCEnumDescriptor ProtobufCEnumDescriptor;
 
-/*
- * ProtobufCEnumValue: this represents a single value of an enumeration.
- * 'name' is the string identifying this value, as given in the .proto file.
- * 'c_name' is the full name of the C enumeration value.
- * 'value' is the number assigned to this value, as given in the .proto file.
- */
+/** Represents a single value of an enumeration. */
 struct _ProtobufCEnumValue {
 	const char	*name;
+	/**< is the string identifying this value, as given in the .proto
+         * file. */
 	const char	*c_name;
+	/**< is the full name of the C enumeration value. */
 	int		value;
+	/**< is the number assigned to this value, as given in the .proto
+         * file. */
 };
 
-/*
- * ProtobufCEnumDescriptor: represents the enum as a whole, with all its values.
- *
- * 'magic' is a code we check to ensure that the api is used correctly.
- * 'name' is the qualified name (e.g. "namespace.Type").
- * 'short_name' is the unqualified name ("Type"), as given in the .proto file.
- * 'package_name' is the '.'-separated namespace
- * 'n_values' is the number of distinct values.
- * 'values' is the array of distinct values.
- * 'n_value_names' number of named values (including aliases).
- * 'value_names' are the named values (including aliases).
+/** Represents the enum as a whole, with all its values.
  *
  * The rest of the values are private essentially.
  *
@@ -234,28 +278,42 @@ struct _ProtobufCEnumValue {
  */
 struct _ProtobufCEnumDescriptor {
 	uint32_t			magic;
+	/**< Code we check to ensure that the api is used correctly. */
 
 	const char			*name;
+	/**< The qualified name (e.g. "namespace.Type"). */
 	const char			*short_name;
+	/**< The unqualified name ("Type"), as given in the .proto file. */
 	const char			*c_name;
 	const char			*package_name;
+	/**< The '.'-separated namespace */
 
 	/* sorted by value */
 	unsigned			n_values;
+	/**< The number of distinct values. */
 	const ProtobufCEnumValue	*values;
+	/**< The array of distinct values. */
 
 	/* sorted by name */
 	unsigned			n_value_names;
+	/**< Number of named values (including aliases). */
 	const ProtobufCEnumValueIndex	*values_by_name;
+	/**< Are the named values (including aliases). */
 
 	/* value-ranges, for faster lookups by number */
 	unsigned			n_value_ranges;
+	/**< Number of value ranges. */
 	const ProtobufCIntRange		*value_ranges;
+	/**< Value ranges. */
 
 	void				*reserved1;
+	/**< Reserved for future use in the implementation. */
 	void				*reserved2;
+	/**< Reserved for future use in the implementation. */
 	void				*reserved3;
+	/**< Reserved for future use in the implementation. */
 	void				*reserved4;
+	/**< Reserved for future use in the implementation. */
 };
 
 /* --- messages --- */
@@ -265,40 +323,41 @@ typedef struct _ProtobufCFieldDescriptor ProtobufCFieldDescriptor;
 typedef struct _ProtobufCMessage ProtobufCMessage;
 typedef void (*ProtobufCMessageInit)(ProtobufCMessage *);
 
-/*
- * ProtobufCFieldDescriptor: description of a single field in a message.
- *
- * 'name' is the name of the field, as given in the .proto file.
- * 'id' is the code representing the field, as given in the .proto file.
- * 'label' is one of PROTOBUF_C_LABEL_{REQUIRED,OPTIONAL,REPEATED}
- * 'type' is the type of field.
- * 'quantifier_offset' is the offset in bytes into the message's C structure
- *        for this member's "has_MEMBER" field (for optional members) or
- *        "n_MEMBER" field (for repeated members).
- * 'offset' is the offset in bytes into the message's C structure
- *        for the member itself.
- * 'descriptor' is a pointer to a ProtobufC{Enum,Message}Descriptor
- *        if type is PROTOBUF_C_TYPE_{ENUM,MESSAGE} respectively,
- *        otherwise NULL.
- * 'default_value' is a pointer to a default value for this field,
- *        where allowed.
- * 'flags' is a flag word. Zero or more of the bits defined in the
- *        ProtobufCFieldFlag enum may be set.
- */
+/** Description of a single field in a message. */
+
 struct _ProtobufCFieldDescriptor {
 	const char		*name;
+	/**< Name of the field, as given in the .proto file. */
+
 	uint32_t		id;
+	/**< Code representing the field, as given in the .proto file. */
 	ProtobufCLabel		label;
+	/**< One of \c PROTOBUF_C_LABEL_{REQUIRED,OPTIONAL,REPEATED} */
 	ProtobufCType		type;
+	/**< The type of field. */
 	unsigned		quantifier_offset;
+	/**< The offset in bytes into the message's C structure's
+         * \c has_MEMBER field (for optional members) or
+         * \c n_MEMBER field (for repeated members). */
 	unsigned		offset;
+	/**< The offset in bytes into the message's C structure
+         * for the member itself. */
 	const void		*descriptor; /* for MESSAGE and ENUM types */
+	/**< Pointer to a \c ProtobufC{Enum,Message}Descriptor if type
+         * is \c PROTOBUF_C_TYPE_{ENUM,MESSAGE} respectively, otherwise
+         * NULL. */
 	const void		*default_value; /* can be NULL */
+	/**< pointer to a default value for this field, where allowed. */
 	uint32_t		flags;
+	/**< A flag word.  Zero or more of the bits defined in the
+         * \c ProtobufCFieldFlag enum may be set. */
 
 	unsigned		reserved_flags;
+	/**< Reserved for future use in the implementation. */
 	void			*reserved2;
+	/**< Reserved for future use in the implementation. */
 	void			*reserved3;
+	/**< Reserved for future use in the implementation. */
 };
 
 typedef enum {
@@ -309,91 +368,116 @@ typedef enum {
 	PROTOBUF_C_FIELD_FLAG_DEPRECATED	= (1 << 1),
 } ProtobufCFieldFlag;
 
-/*
- * ProtobufCMessageDescriptor: description of a message.
+/** Description of a message.
  *
- * 'magic' is a code we check to ensure that the api is used correctly.
- * 'name' is the qualified name (e.g. "namespace.Type").
- * 'short_name' is the unqualified name ("Type"), as given in the .proto file.
- * 'c_name' is the c-formatted name of the structure
- * 'package_name' is the '.'-separated namespace
- * 'sizeof_message' is the size in bytes of the C structure
- *        representing an instance of this type of message.
- * 'n_fields' is the number of known fields in this message.
- * 'fields' is the fields sorted by id number.
- * 'fields_sorted_by_name', 'n_field_ranges' and 'field_ranges'
- *       are used for looking up fields by name and id. (private)
+ * Describes the components of a specific message.
+ *
  */
 struct _ProtobufCMessageDescriptor {
 	uint32_t			magic;
+	/**< Code we check to ensure that the api is used correctly. */
 
 	const char			*name;
+	/**< The qualified name (e.g. "namespace.Type"). */
 	const char			*short_name;
+	/**< The unqualified name ("Type"), as given in the .proto
+         * file. */
 	const char			*c_name;
+	/**< The c-formatted name of the structure */
 	const char			*package_name;
+	/**< The '.'-separated namespace */
 
 	size_t				sizeof_message;
+	/**< The size in bytes of the C structure representing an
+         * instance of this type of message. */
 
 	/* sorted by field-id */
 	unsigned			n_fields;
+	/**< The number of known fields in this message. */
 	const ProtobufCFieldDescriptor	*fields;
+	/**< The fields sorted by id number. */
 	const unsigned			*fields_sorted_by_name;
+	/**< Used for looking up fields by name. (private) */
 
 	/* ranges, optimization for looking up fields */
 	unsigned			n_field_ranges;
+	/**< Number of field ranges. (private) */
 	const ProtobufCIntRange		*field_ranges;
+	/**< Used for looking up fields by id. (private) */
 
 	ProtobufCMessageInit		message_init;
+	/** Function to initialise the message. */
 	void				*reserved1;
+	/**< Reserved for future use in the implementation. */
 	void				*reserved2;
+	/**< Reserved for future use in the implementation. */
 	void				*reserved3;
+	/**< Reserved for future use in the implementation. */
 };
 
-/*
- * ProtobufCMessage: an instance of a message.
- *
- * ProtobufCMessage is sort of a light-weight base class for all messages.
- *
- * In particular, ProtobufCMessage doesn't have any allocation policy
- * associated with it. That's because it's common to create ProtobufCMessage's
- * on the stack. In fact, that's what we recommend for sending messages
- * (because if you just allocate from the stack, then you can't really have a
- * memory leak).
- *
- * This means that functions like protobuf_c_message_unpack() which return a
- * ProtobufCMessage must be paired with a free function, like
- * protobuf_c_message_free_unpacked().
- *
- * 'descriptor' gives the locations and types of the members of message.
- * 'n_unknown_fields' is the number of fields we didn't recognize.
- * 'unknown_fields' are fields we didn't recognize.
- */
 typedef struct _ProtobufCMessageUnknownField ProtobufCMessageUnknownField;
+/** An instance of a message.
+ *
+ * \c ProtobufCMessage is sort of a light-weight base class for all
+ * messages.
+ *
+ * In particular, \c ProtobufCMessage doesn't have any allocation policy
+ * associated with it. That's because it's common to create
+ * \c ProtobufCMessage's on the stack. In fact, that's what we recommend
+ * for sending messages (because if you just allocate from the stack,
+ * then you can't really have a memory leak).
+ *
+ * This means that functions like protobuf_c_message_unpack() which return
+ * a \c ProtobufCMessage must be paired with a free function, like
+ * protobuf_c_message_free_unpacked().
+ */
 struct _ProtobufCMessage {
 	const ProtobufCMessageDescriptor	*descriptor;
+	/**< The locations and types of the members of message. */
 	unsigned				n_unknown_fields;
+	/**< The number of fields we didn't recognize. */
 	ProtobufCMessageUnknownField		*unknown_fields;
+	/**< The fields we didn't recognize. */
 };
 
+/** Initialise message. */
 #define PROTOBUF_C_MESSAGE_INIT(descriptor) { descriptor, 0, NULL }
 
-/*
- * To pack a message: you have two options:
- * (1) you can compute the size of the message using
- *     protobuf_c_message_get_packed_size() then pass
- *     protobuf_c_message_pack() a buffer of that length.
+/** Get the size of the serialised message.
  *
- * (2) Provide a virtual buffer (a ProtobufCBuffer) to
- *     accept data as we scan through it.
+ * Determine the number of bytes required to store the serialised message.
+ *
+ * \param[in] message Message to serialise.
+ * \return Bytes required to store the serialised message.
  */
 PROTOBUF_C_API size_t
-protobuf_c_message_get_packed_size(const ProtobufCMessage *);
+protobuf_c_message_get_packed_size(const ProtobufCMessage *message);
 
+/** Serialise the message.
+ *
+ * Note: The out parameter must have enough space to store the packed
+ * message.  Use protobuf_c_message_get_packed_size() to determine the
+ * size.
+ *
+ * \param[in] message Message to serialise.
+ * \param[in,out] out Buffer to store the serialised message in.
+ * \return Bytes used for the serialised message.
+ */
 PROTOBUF_C_API size_t
-protobuf_c_message_pack(const ProtobufCMessage *, uint8_t *out);
+protobuf_c_message_pack(const ProtobufCMessage *message, uint8_t *out);
 
+/** Serialise the message.
+ *
+ * This function will allocate the space in a buffer required to store
+ * the message.
+ *
+ * \param[in] message Message to serialise.
+ * \param[in,out] out Buffer to store the serialised message in.
+ * \return Bytes used for the serialised message.
+ */
 PROTOBUF_C_API size_t
-protobuf_c_message_pack_to_buffer(const ProtobufCMessage *, ProtobufCBuffer *);
+protobuf_c_message_pack_to_buffer(const ProtobufCMessage *message,
+    ProtobufCBuffer *out);
 
 PROTOBUF_C_API ProtobufCMessage *
 protobuf_c_message_unpack(
@@ -408,12 +492,18 @@ protobuf_c_message_free_unpacked(ProtobufCMessage *, ProtobufCAllocator *);
 PROTOBUF_C_API protobuf_c_boolean
 protobuf_c_message_check(const ProtobufCMessage *);
 
-/*
+/** Initialise message.
+ *
  * WARNING: 'message' must be a block of memory of size
  * descriptor->sizeof_message.
+ *
+ * \param[in] descriptor Message descriptor.
+ * \param[in,out] message Buffer where the \c ProtobufCMessage will be
+ *                        stored.
  */
 PROTOBUF_C_API void
-protobuf_c_message_init(const ProtobufCMessageDescriptor *, void *message);
+protobuf_c_message_init(const ProtobufCMessageDescriptor *descriptor,
+    void *message);
 
 /* --- services --- */
 
@@ -529,18 +619,25 @@ do {									\
 	}								\
 } while (0)
 
-/* ====== private ====== */
+/** @} */  /* End of api group. */
 
+/** \defgroup cgapi Codegen API
+ *
+ * Available for users of \c libprotobuf-c but tied to the current
+ * implementation of the compiler; might change in the future.
+ * @{ */
 #define PROTOBUF_C_SERVICE_DESCRIPTOR_MAGIC	0x14159bc3
 #define PROTOBUF_C_MESSAGE_DESCRIPTOR_MAGIC	0x28aaeef9
 #define PROTOBUF_C_ENUM_DESCRIPTOR_MAGIC	0x114315af
 
-/*
- * A little enum helper macro: this will ensure that your enum's size is
- * sizeof(int). In protobuf, it need not be larger than 32-bits. This is
- * written assuming it is appended to a list w/o a tail comma.
- */
 #ifndef _PROTOBUF_C_FORCE_ENUM_TO_BE_INT_SIZE
+/** Make sure enum isn't too big.
+ *
+ * A little enum helper macro: this will ensure that your enum's size
+ * is * sizeof(int). In protobuf, it needs to not be larger than
+ * 32-bits. This is written assuming it is appended to a list w/o a
+ * tail comma.
+ */
  #define _PROTOBUF_C_FORCE_ENUM_TO_BE_INT_SIZE(enum_name) \
   , _##enum_name##_IS_INT_SIZE = INT_MAX
 #endif
@@ -608,6 +705,6 @@ protobuf_c_service_invoke_internal(
 
 PROTOBUF_C_END_DECLS
 
-/** @} */  /* End of api group. */
+/** @} */  /* End of cgapi group. */
 
 #endif /* __PROTOBUF_C_RUNTIME_H_ */
