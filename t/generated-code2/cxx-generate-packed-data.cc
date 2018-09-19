080724364a5aad61728f8eba57677467bf329088
storyun <joowoni91@gmail.com>
2018-09-19 11:34:48 +0900

    Invalid namespace
    google::protobuf::message::Reflaction is not exist namespace.

--- t/generated-code2/cxx-generate-packed-data.cc
+++ t/generated-code2/cxx-generate-packed-data.cc
@@ -998,7 +998,7 @@ static void dump_test_packed_repeated_enum (void)
 static void dump_test_unknown_fields (void)
 {
   EmptyMess mess;
-  const google::protobuf::Message::Reflection *reflection = mess.GetReflection();
+  const google::protobuf::Reflection *reflection = mess.GetReflection();
   google::protobuf::UnknownFieldSet *fs = reflection->MutableUnknownFields(&mess);
 
 #if GOOGLE_PROTOBUF_VERSION >= 2001000
