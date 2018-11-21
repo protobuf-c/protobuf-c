fcb4410d04c8bd2b52873d73849f77b169255477
Naveen Gogineni <naveen.gogineni@viasat.com>
2018-11-21 14:56:44 -0500

    Fix crash due to removal of proto file

--- t/parser/test-parser.c
+++ t/parser/test-parser.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2018, Naveen Gogineni and the protobuf-c authors.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #include <string.h>
 #include <stdio.h>
 #include "protobuf-c.h"
@@ -270,7 +298,7 @@ int find_enum_descriptor(const ProtobufCFileDescriptor* desc, const ProtobufCEnu
 int main(int argc, char* argv[])
 {   
     int retval = 1;
-    const ProtobufCFileDescriptor* desc = protobuf_c_proto_load_file("t/test-parser.proto", NULL);
+    const ProtobufCFileDescriptor* desc = protobuf_c_proto_load_file("t/test.proto", NULL);
     const ProtobufCMessageDescriptor* mdesc[] = 
     {
         &foo__person__descriptor,
@@ -286,6 +314,12 @@ int main(int argc, char* argv[])
 
     do
     {
+        if( desc == NULL )
+        {
+            printf("Couldnt load file\n");
+            break;
+        }
+
         for( unsigned i = 0; i < sizeof(mdesc)/sizeof(mdesc[0]); i++ )
         {
             if(find_message_descriptor(desc, mdesc[i]) != 0 )
